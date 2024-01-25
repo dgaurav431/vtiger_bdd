7c8ddb6a6d45095b184a809f7c4695ea4ea7c1e7
akashdeb <akashdeb87@gmail.com>
2024-01-25 12:43:00 +0530

    Conflict

--- vtiger/src/test/java/JDBC/DemoClass.java
+++ vtiger/src/test/java/JDBC/DemoClass.java
@@ -7,6 +7,8 @@ public class DemoClass {
 		System.out.println("Demo Push From New Branch");
 		System.out.println("Demo Changes in Global");
 		System.out.println("Demo Changes 2 in Global");
+		System.out.println("Demo Changes 2.5 in Local");
+
 
 
 
